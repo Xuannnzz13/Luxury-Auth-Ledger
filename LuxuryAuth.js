14f4916accc90062925a2a918783dd2a128554a1
Xuannnzz13 <243019J@mymail.nyp.edu.sg>
2026-01-29 11:02:46 +0800

    change the getreadonlyprovider

--- LuxuryAuth.js
+++ LuxuryAuth.js
@@ -115,7 +115,7 @@ async function getContract() {
   );
 }
 function getReadOnlyProvider() {
-  return new ethers.JsonRpcProvider("https://rpc.https://ethereum-sepolia-rpc.publicnode.com");
+  return new ethers.JsonRpcProvider("https://ethereum-sepolia-rpc.publicnode.com");
 }
 
 function getReadOnlyContract() {
